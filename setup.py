2f8e9b723b3a44ffd26acbbdd84d5e3da5c7a2f1
Günther Neumair <g.neumair@commend.com>
2015-03-18 13:36:55 +0100

    Setup tools, restructuring

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+setup(
+    name = "MultiWiiSerialUAV",
+    version = "0.1",
+    packages = find_packages(),
+
+
+    author = "Guenther Neumair",
+    author_email = "g.neumair@yahoo.com",
+    description = "Serial interface for remote controlling a multiwii flight controller",
+    license = "GPL",
+    keywords = "multiwii serial uav",
+)
